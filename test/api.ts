64505c4afb6c4b263ec3c62d4470770d0265f72c
Tobias Bleckert <hola@tobiasbleckert.se>
2024-05-24 21:03:21 +0200

    No need for fetch polyfill

--- test/api.ts
+++ test/api.ts
@@ -1,10 +1,5 @@
 // @ts-ignore
-import fetch from 'node-fetch';
 import Minutemailer from '../src/Minutemailer';
-
-if (!globalThis.fetch) {
-    globalThis.fetch = fetch;
-}
 
 const minutemailer = new Minutemailer(process.env.MINUTEMAILER_API_KEY || 'nokey', 'http://api-gateway.minutemailer.test');
 
